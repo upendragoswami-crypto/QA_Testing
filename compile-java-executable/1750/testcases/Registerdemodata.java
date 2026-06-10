1bd9bc272e00f3831c9d2a68fba7a2f1b88d4fbd
Upendra cotestertest2 <upendra.goswami+8@testgrid.io>
2026-06-10 16:40:49 +0100

    Initial Version Commit

---
+++ compile-java-executable/1750/testcases/Registerdemodata.java
@@ -0,0 +1,34 @@
+import io.testgrid.listeners.TestListener;
+import io.testgrid.listeners.RetryFailedTestCases;
+import io.testgrid.tg;
+import org.testng.annotations.*;
+import app.getxray.xray.testng.annotations.XrayTest;
+import io.testgrid.enums.ComparisonType;
+import org.json.JSONObject;
+import io.testgrid.enums.Direction;
+import io.testgrid.enums.Size;
+import io.testgrid.enums.Buttons;
+import static io.testgrid.baseClass.driver;
+import org.openqa.selenium.*;
+import static io.testgrid.enums.KeyboardKeys.*;
+import org.openqa.selenium.support.ui.Select;
+import java.net.*;
+import java.util.*;
+import java.io.*;
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.Test;
+
+@Listeners(TestListener.class);
+public class registerdemodata {
+
+	@Test(retryAnalyzer = RetryFailedTestCases.class)
+	public void registerdemodata() {
+		tg.openBrowser();
+		tg.wait("ele_firstname556", ComparisonType.IS_VISIBLE);
+		tg.click("ele_firstname556", 1);
+		tg.wait("ele_firstname556", ComparisonType.IS_VISIBLE);
+		tg.type("ele_firstname556", "hiii");
+		tg.close();
+	}
+}
