8bc2117b078fb0b8a705008ac82d8385ec027bb3
Upendra Cloud <upendra.goswami+13@testgrid.io>
2026-07-14 11:55:09 +0000

    TC

---
+++ compile-java-executable/222/testcases/UR.java
@@ -0,0 +1,27 @@
+import io.testgrid.listeners.TestListener;
+import io.testgrid.listeners.RetryFailedTestCases;
+import io.testgrid.tg;
+import org.testng.annotations.*;
+import app.getxray.xray.testng.annotations.XrayTest;
+import io.testgrid.enums.ComparisonType;
+import org.json.JSONObject;
+import io.testgrid.enums.Direction;
+import io.testgrid.enums.Size;
+import io.testgrid.enums.Buttons;
+import static io.testgrid.baseClass.driver;
+import org.openqa.selenium.*;
+import io.testgrid.enums.Alert;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.ios.IOSDriver;
+import org.testng.annotations.Test;
+
+@Listeners(TestListener.class);
+public class ur {
+
+	@Test(retryAnalyzer = RetryFailedTestCases.class)
+	public void ur() {
+		tg.openDevice();
+		tg.wait(5);
+		tg.close();
+	}
+}
